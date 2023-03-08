0e4e94846f9ce4e8e20bd5f871d2f03ec933f3e0
Sota Nakamura <49087984+sotanakamura@users.noreply.github.com>
2023-03-08 14:48:51 +0900

    Add files via upload

---
+++ build.cmd
@@ -0,0 +1,4 @@
+nuget install Microsoft.Windows.CppWinRT -OutputDirectory packages
+nuget install Microsoft.WindowsAppSDK -OutputDirectory packages 
+packages\Microsoft.Windows.CppWinRT.2.0.230225.1\bin\cppwinrt.exe -optimize -input packages\Microsoft.WindowsAppSDK.1.2.230217.4\lib\uap10.0 -input packages\Microsoft.WindowsAppSDK.1.2.230217.4\lib\uap10.0.18362 -input sdk -output "Generated Files"
+cl /I"packages\Microsoft.WindowsAppSDK.1.2.230217.4\include" /I"Generated Files" /EHsc /std:c++17 main.cpp /link /LIBPATH:"packages\Microsoft.WindowsAppSDK.1.2.230217.4\lib\win10-x64" Microsoft.WindowsAppRuntime.Bootstrap.lib Microsoft.WindowsAppRuntime.lib WindowsApp.lib /SUBSYSTEM:WINDOWS /MANIFEST:EMBED
